924524794f6fa87444addaa0531e4c5a5e9e0fe4
prasad42 <prasadpawar42@protonmail.com>
2024-11-23 23:48:08 +0530

    parameters update

--- script.sh
+++ script.sh
@@ -5,11 +5,11 @@
 #SBATCH --job-name=2siteBH
 #SBATCH --error=job.%J.err_node_48 
 #SBATCH --output=job.%J.out_node_48 
-#SBATCH --partition=highmemory
+#SBATCH --partition=small
 
 # Activate the virtual environment
 source /home/apps/DL/DL-CondaPy3.7/etc/profile.d/conda.sh
 conda activate qutip
 
 # Run your Python script
-python3 calc_SFF_open.py
+python3 calc_SFF.py
